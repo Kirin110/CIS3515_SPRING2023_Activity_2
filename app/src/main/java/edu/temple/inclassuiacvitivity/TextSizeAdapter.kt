920bd9af74cc2d59dd5b48b8b40104456e5be03d
Kirin <tuj17819@temple.edu>
2023-02-09 12:20:11 -0500

    Implement getView and getDropDownView

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,24 +1,48 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(_context: Context, _textSizes: Array<Int>) : BaseAdapter() {
+
+    val context = _context
+    val textSizes = _textSizes
+
+    //Returns the amount of views it can generate
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return textSizes.size
     }
 
+    //Gets the item in the index or position of the array
     override fun getItem(p0: Int): Any {
-        TODO("Not yet implemented")
+        return textSizes[p0]
     }
 
-    override fun getItemId(p0: Int): Long {
-        TODO("Not yet implemented")
-    }
+    //No need to implement this function since we do not need a unique id
+    override fun getItemId(p0: Int) = p0.toLong()
 
     override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        TODO("Not yet implemented")
+        return (getDropDownView(p0, p1, p2) as TextView).apply {
+            textSize = 22f
+        }
+    }
+    override fun getDropDownView(p0: Int, p1: View?, parent: ViewGroup?): View {
+        val textView: TextView
+
+        if(p1 == null) {
+            textView = TextView(context)
+            textView.setPadding(10, 20, 0, 20)
+        } else {
+            textView = p1 as TextView
+        }
+        //Set the text sizes of the font
+        textView.textSize = textSizes[p0].toFloat()
+        textView.text = textSizes[p0].toString()
+
+        return textView
     }
 
 }
